715c8a2901c5abfb88f5a5be5663e03945831ff9
Sudolsky Lukas <lukas.sudolsky@centrum.cz>
2014-02-09 10:52:41 +0100

    HelloWorld extension

---
+++ www/app/code/local/ChSoft/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+class ChSoft_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action
+{
+	public function indexAction()
+	{  
+		$this->loadLayout(array('default'));
+		$this->renderLayout(); 
+	}
+
+}
+?>
